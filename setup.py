e005780f80239696ee5c469958e4da356cd23016
rachelbastian <116000973+clothifyai@users.noreply.github.com>
2022-12-02 09:32:33 -0600

    Version 28 - Final Accepts Pw

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='mlq',
-    version='0.2.7',
+    version='0.2.8',
     packages=['mlq', 'controller'],
     long_description=open('README.txt').read(),
     install_requires=open('requirements.txt').read(),
